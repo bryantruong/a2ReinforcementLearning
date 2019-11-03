3f0a9ef78aec32cea54860f5a13d2be7a9363f7c
bryantruong <btruong@live.unc.edu>
2019-11-03 17:54:46 -0500

    renamed file, added getAvailablePositions helper

--- learn4x4x4.py
+++ learn4x4x4.py
@@ -13,8 +13,102 @@ def getAvailablePositions(gameBoard):
         for row in range(4):
             for col in range(4):
                 if gameBoard[matrix][row][col] == 0:
-                    positions.append((matrix,row,col))
+                    positions.append((matrix, row, col))
     return positions
+
+
+def checkIfWinner(gameBoard, lastPlacedMatrix, lastPlacedRow, lastPlacedCol):
+
+    # check along current matrix's row
+    rowSum = 0
+    for col in range(4):
+        rowSum += gameBoard[lastPlacedMatrix][lastPlacedRow][col]
+    if rowSum == 4 or rowSum == -4:
+        return True
+    # check along current matrix's col
+    colSum = 0
+    for row in range(4):
+        colSum += gameBoard[lastPlacedMatrix][row][lastPlacedCol]
+    if colSum == 4 or colSum == -4:
+        return True
+    # check along z axis
+    zSum = 0
+    for matrix in range(4):
+        zSum += gameBoard[matrix][lastPlacedRow][lastPlacedCol]
+    if zSum == 4 or zSum == -4:
+        return True
+
+    currentMatrix = gameBoard[lastPlacedMatrix]
+    # check the sum of a diagonal of a matrix, from top left to bottom right (the trace)
+    zDiagonalSum1 = np.trace(currentMatrix)
+    if zDiagonalSum1 == 4 or zDiagonalSum1 == -4:
+        return True
+    # check the sum of a diagonal of a matrix, from top right to bottom left
+    zDiagonalSum2 = currentMatrix[0][3] + currentMatrix[1][2] + currentMatrix[2][1] + currentMatrix[3][0]
+    if zDiagonalSum2 == 4 or zDiagonalSum2 == -4:
+        return True
+
+    # Checking across matrices for diagonals where one variable can be held constant (per iteration)
+    for index in range(4):
+        xDiagonalSum1 = gameBoard[0][index][0] + gameBoard[1][index][1] + gameBoard[2][index][2] + gameBoard[3][index][
+            3]
+        xDiagonalSum2 = gameBoard[3][index][0] + gameBoard[2][index][1] + gameBoard[1][index][2] + gameBoard[0][index][
+            3]
+        yDiagonalSum1 = gameBoard[0][3][index] + gameBoard[1][2][index] + gameBoard[2][1][index] + gameBoard[3][0][
+            index]
+        yDiagonalSum2 = gameBoard[0][0][index] + gameBoard[1][1][index] + gameBoard[2][2][index] + gameBoard[3][3][
+            index]
+
+    if xDiagonalSum1 == 4 or xDiagonalSum2 == 4 or yDiagonalSum1 == 4 or yDiagonalSum2 == 4 or \
+            xDiagonalSum1 == -4 or xDiagonalSum2 == -4 or yDiagonalSum1 == -4 or yDiagonalSum2 == -4:
+        return True
+
+    # Hardcoded to check for four different internal diagonals
+    if gameBoard[0][0][0] + gameBoard[1][1][1] + gameBoard[2][2][2] + gameBoard[3][3][3] == 4 or \
+            gameBoard[0][0][0] + gameBoard[1][1][1] + gameBoard[2][2][2] + gameBoard[3][3][3] == -4:
+        return True
+
+    if gameBoard[0][0][3] + gameBoard[1][1][2] + gameBoard[2][2][1] + gameBoard[3][3][0] == 4 or \
+            gameBoard[0][0][3] + gameBoard[1][1][2] + gameBoard[2][2][1] + gameBoard[3][3][0] == -4:
+        return True
+
+    if gameBoard[0][3][3] + gameBoard[1][2][2] + gameBoard[2][1][1] + gameBoard[3][0][0] == 4 or \
+            gameBoard[0][3][3] + gameBoard[1][2][2] + gameBoard[2][1][1] + gameBoard[3][0][0] == -4:
+        return True
+
+    if gameBoard[0][3][0] + gameBoard[1][2][1] + gameBoard[2][1][2] + gameBoard[3][0][3] == 4 or \
+            gameBoard[0][3][0] + gameBoard[1][2][1] + gameBoard[2][1][2] + gameBoard[3][0][3] == - 4:
+        return True
+    return False
+
+
+def findBestPosition():
+    # TODO: use np.argmax(?) to find the index of the highest utility value, if there is a tie, choose one from random
+    return -1, -1, -1
+
+
+def playGame(existingUtilityMatrix, gameBoard):
+    winningPlayer = None
+    while True:
+        availablePositions = getAvailablePositions(gameBoard)
+        bestPosition = findBestPosition(availablePositions)
+        gameBoard[bestPosition] = 1
+        if checkIfWinner(gameBoard, bestPosition[0], bestPosition[1], bestPosition[2]):
+            winningPlayer = 1
+            break
+        availablePositions = getAvailablePositions(gameBoard)
+        bestPosition = findBestPosition(availablePositions)
+        gameBoard[bestPosition] = -1
+        if checkIfWinner(gameBoard, bestPosition[0], bestPosition[1], bestPosition[2]):
+            winningPlayer = -1
+            break
+
+        # TODO: Loop through gameboard and if gameboard at i,j,k == winningPlayer, increase its utility cell by 1
+
+
+
+
+
 
 
 
@@ -22,6 +116,7 @@ if __name__ == '__main__':
     # Check to ensure the program was run correctly
     if len(sys.argv) != 4:
         print("incorrect number of arguments, exiting.")
+        sys.exit()
     else:
         numOfTrials1 = int(sys.argv[1])
         numOfTrials2 = int(sys.argv[2])
@@ -29,16 +124,26 @@ if __name__ == '__main__':
         utilityMatrix = np.zeros((4, 4, 4))  # initialize a utilityMatrix
         # 4 matrices, each with 4 rows and 4 columns
         print(utilityMatrix)
+
+
         for i in range(numOfTrials1):
-            gameMatrix = np.zeros((4, 4, 4))  # initialize the game board
-            # TODO: Do the reinforcement learning, then print utility values
-            # Probably make a method for "playing a game"
-            break
+            gameBoard = np.zeros((4, 4, 4))  # initialize a utilityMatrix
+            utilityMatrix = playGame(utilityMatrix, gameBoard)
 
+        # TODO: Divide each cell in utility matrix by numOfTrials1
+        print(utilityMatrix)
+        # TODO: Multiply each cell in utility matrix by numOfTrials1
         for i in range(numOfTrials1, numOfTrials2):
-            # TODO: Do the reinforcement learning, then print utility values
-            break
-        for i in range(numOfTrials2, numOfTrials3):
-            # TODO: Do the reinforcement learning, then print utility values
-            break
+            gameBoard = np.zeros((4, 4, 4))  # initialize a utilityMatrix
+            utilityMatrix = playGame(utilityMatrix, gameBoard)
 
+        # TODO: Divide each cell in utility matrix by numOfTrials2
+        print(utilityMatrix)
+        # TODO: Multiply each cell in utility matrix by numOfTrials2
+
+        for i in range(numOfTrials2, numOfTrials3):
+            gameBoard = np.zeros((4, 4, 4))  # initialize a utilityMatrix
+            utilityMatrix = playGame(utilityMatrix, gameBoard)
+
+        # TODO: Divide each cell in utility matrix by numOfTrials3
+        print(utilityMatrix)
